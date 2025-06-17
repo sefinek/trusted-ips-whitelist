90527bd2fde6063baf545b7b4284602c98326edc
Sefinek <contact@sefinek.net>
2025-06-18 00:19:04 +0200

    timeout: 60000

--- scripts/services/axios.js
+++ scripts/services/axios.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const { version } = require('../../package.json');
 
 const api = axios.create({
-	timeout: 25000,
+	timeout: 60000,
 	headers: {
 		'User-Agent': `Mozilla/5.0 (compatible; GoodBots-IP-Whitelist/${version}; +https://github.com/sefinek/GoodBots-IP-Whitelist)`,
 		'Accept': 'application/json',
