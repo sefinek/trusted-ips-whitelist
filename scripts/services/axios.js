49b8004d40dd8c6ac9bb2e108c30690bde1933dd
anupamme <mediratta@gmail.com>
2026-08-10 18:32:21 +0000

    fix: V-001 security vulnerability
    Automated security fix generated by OrbisAI Security

--- scripts/services/axios.js
+++ scripts/services/axios.js
@@ -1,8 +1,12 @@
 const axios = require('axios');
+const http = require('node:http');
+const https = require('node:https');
 const { version } = require('../../package.json');
 
 const api = axios.create({
 	timeout: 60000,
+	httpAgent: new http.Agent({ maxSockets: 10 }),
+	httpsAgent: new https.Agent({ maxSockets: 10 }),
 	headers: {
 		'User-Agent': `Mozilla/5.0 (compatible; KnownIPsWhitelist/${version}; +https://github.com/sefinek/trusted-ips-whitelist)`,
 		'Accept': 'application/json',
